6fc5cdeec0f4bb7fa92ba4f73d0459fb2149b0f4
Gavin He <hscgavin@gmail.com>
2016-02-26 17:44:34 +1100

    added layout, route and schema

---
+++ collections/recipes.js
@@ -0,0 +1,27 @@
+Recipes = new Mongo.Collection('recipes');
+RecipeSchema = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Name"
+    },
+    desc: {
+        type: String,
+        label: "Description"
+    },
+    author:{
+        type: String,
+        label: "Author",
+        autoValue: function() {
+            return this.userId;
+        }
+    },
+    createdAt: {
+        type: String,
+        label: "Created At",
+        autoValue: function() {
+            return new Date();
+        }
+    }
+});
+
+Recipes.attachSchema(RecipeSchema);
