ed56be4018d24bc5d92568709a47bb6b6b3a0ed6
RMITGavin <s3418864@student.rmit.edu.au>
2016-02-29 00:38:10 +1100

    updated recipes schema and template

--- collections/recipes.js
+++ collections/recipes.js
@@ -1,9 +1,19 @@
-Recipes = new Meteor.Collection('recipes');
+Recipes = new Mongo.Collection('recipes');
 Recipes.allow({
     insert: function(userId, doc) {
         return !!userId;
     }
 });
+
+Ingredient = new SimpleSchema({
+    name: {
+        type: String
+    },
+    amount: {
+        type: String
+    }
+});
+
 RecipeSchema = new SimpleSchema({
     name: {
         type: String,
@@ -12,6 +22,14 @@ RecipeSchema = new SimpleSchema({
     desc: {
         type: String,
         label: "Description"
+    },
+    ingredients: {
+        type : [Ingredient]
+    },
+    inMenu: {
+        type: Boolean,
+        defaultValue: false,
+        optional: true
     },
     author:{
         type: String,
