7c2c298a5c9d439f966df1554b7ccd08d3db9f5a
RMITGavin <s3418864@student.rmit.edu.au>
2016-03-02 23:54:17 +1100

    Added single recipe template, helper and href

---
+++ client/recipes/RecipeSingle.js
@@ -0,0 +1,15 @@
+//Meteor.subscribe('recipes');
+Template.RecipeSingle.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+        var id = FlowRouter.getParam('id');
+        self.subscribe('singleRecipe', id);
+    })
+});
+
+Template.RecipeSingle.helpers({
+    recipe: ()=> {
+        var id = FlowRouter.getParam('id');
+        return Recipes.findOne({_id:id});
+    }
+});
