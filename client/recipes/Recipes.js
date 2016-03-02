58bbd42a862072e00b587961fb8a7cc06cea576f
RMITGavin <s3418864@student.rmit.edu.au>
2016-03-02 23:53:09 +1100

    added recipes template level subscriptions, publish single recipe, and add route for single recipe

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,4 +1,11 @@
-Meteor.subscribe('recipes');
+//Meteor.subscribe('recipes');
+Template.Recipes.onCreated(function() {
+   var self = this;
+    self.autorun(function(){
+        self.subscribe('recipes');
+    });
+});
+
 Template.Recipes.helpers({
     recipes: ()=> {
         return Recipes.find({});
--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,7 @@
 Meteor.publish('recipes', function() {
     return Recipes.find({author: this.userId});
 });
+Meteor.publish('singleRecipe', function(id) {
+    //check(id, String);
+    return Recipes.find({_id: id});
+});
